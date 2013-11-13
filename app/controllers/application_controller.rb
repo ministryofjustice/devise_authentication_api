4704956774356a7039fb4be7888de09202defd66
Rob McKinnon <@digital.justice.gov.uk>
2013-11-13 15:39:26 +0000

    includes to get JSON handling working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+  include ActionController::MimeResponds # details see: https://github.com/rails-api/rails-api/issues/24
+
+  include ActionController::ImplicitRender
+  include ActionController::StrongParameters
+
+  respond_to :json
+
+  skip_before_action :verify_authenticity_token # disable Cross-Site Request Forgery protection
+
 end
