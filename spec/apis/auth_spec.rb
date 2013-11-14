a4f67eb4dad760eb373cab86177fc8e408d59fdc
robmckinnon <rob@movingflow>
2013-11-14 16:34:10 +0000

    spec JSON returned when registering

--- spec/apis/auth_spec.rb
+++ spec/apis/auth_spec.rb
@@ -3,25 +3,40 @@ require 'json'
 
 describe 'auth api', :type => :api do
 
+  def json_contains key, value
+    JSON.parse(last_response.body)[key].should == value
+  end
+
   context 'Unauthenticated user' do
 
     before :all do
-      @good_creds = {user: {email: 'joe.bloggs@example.com', password: 's3kr!tpa55'} }
+      @email = 'joe.bloggs@example.com'
+      @good_creds = {user: {email: @email, password: 's3kr!tpa55'} }
       @bad_creds = {user: {email: 'bad login'} }
     end
 
     describe 'POST /users.json' do
       describe 'success' do
+        before { post('/users.json', @good_creds) }
+
         it 'returns 201 status code' do
-          post('/users.json', @good_creds)
           last_response.status.should eq 201
+        end
+
+        it 'returns email in JSON' do
+          json_contains 'email', @email
         end
       end
 
       describe 'failure' do
+        before { post('/users.json', @bad_creds) }
+
         it 'returns 400 status code' do
-          post('/users.json', @bad_creds)
           last_response.status.should eq 422
+        end
+
+        it 'returns errors in JSON' do
+          json_contains 'errors', {"email"=>["is invalid"], "password"=>["can't be blank"]}
         end
       end
     end
