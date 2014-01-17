0f31aa382a6b7e72b9cd4d576c3ae8108ac4237c
Rob McKinnon <@digital.justice.gov.uk>
2014-01-17 14:06:17 +0000

    refactoring

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,20 +8,16 @@ class RegistrationsController < Devise::RegistrationsController
     resource.skip_confirmation_notification! # stop email from being sent
 
     if resource.save
-      yield resource if block_given?
       if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
       else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
     else
       clean_up_passwords resource
-      respond_with resource
     end
+
+    respond_with resource, :location => nil
   end
 
 end
