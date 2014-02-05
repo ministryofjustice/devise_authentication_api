fe7262aad42317e4872e079b538718ae58972528
Rob McKinnon <@digital.justice.gov.uk>
2014-02-05 11:28:26 +0000

    spec for confirm API response

---
+++ spec/controllers/confirmations_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe 'confirmation via POST /users/confirmation/:confirmation_token' do
+
+  include ApiHelper
+  include_context 'shared setup'
+
+  context 'by unconfirmed user' do
+    describe 'success' do
+      before do
+        register @good_creds
+        confirmation_token = JSON.parse(last_response.body)['confirmation_token']
+        post "/users/confirmation/#{confirmation_token}"
+      end
+
+      it_behaves_like 'no content success response'
+    end
+  end
+
+  context 'by already confirmed user' do
+    describe 'failure' do
+      before do
+        register @good_creds
+        confirmation_token = JSON.parse(last_response.body)['confirmation_token']
+        post "/users/confirmation/#{confirmation_token}"
+        post "/users/confirmation/#{confirmation_token}"
+      end
+
+      it 'returns 422 Unprocessable Entity status code' do
+        status_code_is 422
+      end
+
+      it 'returns "Invalid token." error' do
+        json_contains 'error', 'Invalid token.'
+      end
+    end
+  end
+
+  context 'with bad confirmation_token' do
+    describe 'failure' do
+      before do
+        post "/users/confirmation/bad_token"
+      end
+
+      it 'returns 422 Unprocessable Entity status code' do
+        status_code_is 422
+      end
+
+      it 'returns "Invalid token." error' do
+        json_contains 'error', 'Invalid token.'
+      end
+    end
+  end
+end
