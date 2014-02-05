67dd783672bdfd858bc17de175104e16a6bcce0a
Rob McKinnon <@digital.justice.gov.uk>
2014-02-05 16:27:59 +0000

    for test and development set config.action_mailer.delivery_method = :test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,5 +19,8 @@ DeviseAuthenticationApi::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  config.action_mailer.delivery_method = :letter_opener
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
 end
